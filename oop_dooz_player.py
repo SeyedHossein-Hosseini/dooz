defe831fb30e999192babd518bdc45c9d158fa32
Hossein <s.h.hosseini.1376@gmail.com>
2024-03-07 15:07:02 +0330

    docstring added

--- oop_dooz_player.py
+++ oop_dooz_player.py
@@ -1,19 +1,28 @@
+
+"""
+Player class that has
+    following attrs => marker, name, score, turn
+    following methods => choose_position, select_marker, cal_score
+"""
 class Player:
     def __init__(self,name):
+        # marker will be 'X' or 'O'
         self.marker = None
+        # name of the player
         self.name = name
         self.score = 0
         self.turn = False
 
-    def choose_position(self, board: Board):
-        # cheker : check input
+    # gets board object from Board class and returns the valid position between 1 and 9 based on the board 
+    def choose_position(self, board):
         while True:
             position=input('Dear '+ self.name +' please choose a position:')
             if position.isdigit():
                 position=int(position)
-                if position in range(1,10) and board.free_space(position): #board command
+                if position in range(1,10) and board.free_space(position): 
                     return position
 
+    # based on player input, it figures the marker and returns the (selected_marker, other_marker)
     def select_marker(self):
       while True:
         player_sign = input(f'Dear {self.name}, choose X or O:')
@@ -24,6 +33,7 @@ class Player:
           self.marker = 'O'
           return ('O', 'X')
         
-    def cal_score(self, checker: Checker, board: Board):
+    # it gets the checker object and board object from Checker and Board classes and checks a player with specific name has won. if yes, the score will be incremented by 1
+    def cal_score(self, checker, board):
        if checker.win_check(board, self.name):
           self.score += 1
